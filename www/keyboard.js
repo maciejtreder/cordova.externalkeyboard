def0a1b55e4cc06131cdc1d8ef4a95a8d6cc61e3
Maciej Treder <contact@maciejtreder.com>
2018-03-18 15:25:10 +0100

    test

--- www/keyboard.js
+++ www/keyboard.js
@@ -6,30 +6,35 @@ var ExternalKeyboard = function() {
 };
 
 ExternalKeyboard.redraw = function(){
-    var deferred = $.Deferred();
-    exec(
-        function (result) {
-            deferred.resolve(result);
-        },
-        function (error) {
-            deferred.resolve(error);
-        },
-        pluginName, "redraw", []);
-    return deferred.promise();
+    // var deferred = $.Deferred();
+    //
+    // exec(callback, function(err) {
+    //     callback('Nothing to echo.');
+    // }
+    //
+    // exec(
+    //     function (result) {
+    //         deferred.resolve(result);
+    //     },
+    //     function (error) {
+    //         deferred.resolve(error);
+    //     },
+    //     pluginName, "redraw", []);
+    // return deferred.promise();
 }
 
 
 ExternalKeyboard.setKeyCommands = function(commands, delimiter){
-    var deferred = $.Deferred();
-    exec(
-        function (result) {
-            deferred.resolve(result);
-        },
-        function (error) {
-            deferred.resolve(error);
-        },
-        pluginName, "setKeyCommands", [commands, delimiter]);
-    return deferred.promise();
+    // var deferred = $.Deferred();
+    // exec(
+    //     function (result) {
+    //         deferred.resolve(result);
+    //     },
+    //     function (error) {
+    //         deferred.resolve(error);
+    //     },
+    //     pluginName, "setKeyCommands", [commands, delimiter]);
+    // return deferred.promise();
 }
 
 
