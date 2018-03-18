f0f465ab037681117187f29a32e3e73a58ad2a3f
Maciej Treder <contact@maciejtreder.com>
2018-03-18 15:29:43 +0100

    test

--- www/keyboard.js
+++ www/keyboard.js
@@ -5,37 +5,37 @@ var pluginName = 'ExternalKeyboard';
 var ExternalKeyboard = function() {
 };
 
-ExternalKeyboard.redraw = function(){
-    // var deferred = $.Deferred();
-    //
-    // exec(callback, function(err) {
-    //     callback('Nothing to echo.');
-    // }
-    //
-    // exec(
-    //     function (result) {
-    //         deferred.resolve(result);
-    //     },
-    //     function (error) {
-    //         deferred.resolve(error);
-    //     },
-    //     pluginName, "redraw", []);
-    // return deferred.promise();
-}
+// ExternalKeyboard.redraw = function(){
+//     // var deferred = $.Deferred();
+//     //
+//     // exec(callback, function(err) {
+//     //     callback('Nothing to echo.');
+//     // }
+//     //
+//     // exec(
+//     //     function (result) {
+//     //         deferred.resolve(result);
+//     //     },
+//     //     function (error) {
+//     //         deferred.resolve(error);
+//     //     },
+//     //     pluginName, "redraw", []);
+//     // return deferred.promise();
+// }
 
 
-ExternalKeyboard.setKeyCommands = function(commands, delimiter){
-    // var deferred = $.Deferred();
-    // exec(
-    //     function (result) {
-    //         deferred.resolve(result);
-    //     },
-    //     function (error) {
-    //         deferred.resolve(error);
-    //     },
-    //     pluginName, "setKeyCommands", [commands, delimiter]);
-    // return deferred.promise();
-}
+// ExternalKeyboard.setKeyCommands = function(commands, delimiter){
+//     // var deferred = $.Deferred();
+//     // exec(
+//     //     function (result) {
+//     //         deferred.resolve(result);
+//     //     },
+//     //     function (error) {
+//     //         deferred.resolve(error);
+//     //     },
+//     //     pluginName, "setKeyCommands", [commands, delimiter]);
+//     // return deferred.promise();
+// }
 
 
 module.exports = ExternalKeyboard;
